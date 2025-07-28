23a50974bd50bb0f7934539071a4998be6471a8f
Parker Bray-Jones <parker@Parkers-MacBook-Air.local>
2025-07-27 21:04:43 -0400

    Update project.py

--- project.py
+++ project.py
@@ -3,7 +3,7 @@ from scipy.integrate import solve_ivp, solve_bvp
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 
-alpha = 1
+alpha = 0.05
 Tinit = 0
 
 def dTdt(t, T):
@@ -11,13 +11,21 @@ def dTdt(t, T):
     R1 = 1
 
     delta_r = R1/(n+1)
-    r = np.linspace(0, R1, n)
+    r = np.linspace(0.00001, R1, n+1)
 
-    T_ice = np.hstack([Tinit, T])
+    T_ice = np.hstack([T, 10])
+
+    dTdt = np.zeros(n+1)
+
+    dTdt[0] = alpha/(delta_r**2) * (2*T[1]-2*T[0])
 
     for i in range(1, n+1):
-        dTdt = alpha/(delta_r**2) * ((T_ice[i+1]-2*T[i]+T_ice[i-1]) + 1/r[i] * (T_ice[i+1]-T[i-1]))
-
+        dTdt[i] = alpha * ((T_ice[i+1]-2*T[i]+T_ice[i-1])/(delta_r**2) + 1/r[i] * (T_ice[i+1]-T_ice[i-1])/(2*delta_r))
+    
     return dTdt
 
-    
+tend = 1000
+sol = solve_ivp(dTdt, (0, tend), np.ones(9)*Tinit)
+
+plt.plot(sol.t, sol.y[100, :])
+
