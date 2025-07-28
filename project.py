f62ddc97469f3b54c409f767b3f43e69800aa1d6
Parker Bray-Jones <prbrayjo@uwaterloo.ca>
2025-07-28 13:50:06 -0400

    should be good

--- project.py
+++ project.py
@@ -3,32 +3,44 @@ from scipy.integrate import solve_ivp, solve_bvp
 from scipy.optimize import fsolve
 import matplotlib.pyplot as plt
 
-alpha = 5
+
 Tinit = 0
+Tf0 = 10
 
 n = 5
 R1 = 3
 R2 = 5
-k = 1
-hi = 1
+A1 = 2*np.pi*R1
+A2 = 2*np.pi*R2
+
+k = 0.22
+hf = 1
 ha = 1
 
-Tf = 10
+rho = 1 # g/cm3
+cp = 4.184 # J/(g * K)
+alpha = k/(rho*cp)
 
-def dTdt(t, T):
+Tinf = 25 # degrees celcius
+
+def dTdt(t, T0):
+   T = T0[:-1]
+   Tf = T0[-1]
    delta_r = R1/(n+1)
    r = np.linspace(0, R1, n+2)
    dTdt = np.zeros(n+2)
-   
+
    dTdt[0] = alpha * (2*T[1]-2*T[0])/(delta_r**2)
    dTdt[1:-1] = alpha * ((T[2:]-2*T[1:-1]+T[:-2])/(delta_r**2) + 1/r[1:-1] * (T[2:]-T[:-2])/delta_r)
-   dTdt[-1] = -k/hi * (T[-1]-Tf)
+   dTdt[-1] = -k/hf * (T[-1]-Tf)
 
-   return dTdt
+   dTfdt = -(ha * A2 * (Tf-Tinf) - hf * A1 * (T[-1]-Tf))/(rho*cp)
 
-tend = 5
+   return np.hstack([dTdt, dTfdt])
 
-sol = solve_ivp(dTdt, (0, tend), np.ones(n+2)*Tinit)
+tend = 10
+
+sol = solve_ivp(dTdt, (0, tend), np.hstack([np.ones(n+2)*Tinit, Tf0]))
 
 y_vals = np.linspace(0, 10, n+2)
 
@@ -39,8 +51,8 @@ soly = sol.y
 plt.figure()
 for i in range(0, tsteps, 2):
 	plt.cla()
-	plt.ylim([0,10])
-	plt.plot(y_vals,soly[:, i], label='t='+str(round(sol.t[i], 2))+'seconds')
+	plt.ylim([0,25])
+	plt.plot(y_vals,soly[:-1, i], label='t='+str(round(sol.t[i], 2))+'seconds')
 	plt.legend()
 	plt.xlabel("r (cm)")
 	plt.ylabel("Temperature (Celcius)")
